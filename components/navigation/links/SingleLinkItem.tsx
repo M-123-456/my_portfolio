02ab9f807a5d76a289ba0449f39c7badf996e261
M-123-456 <mikigerlach2@gmail.com>
2022-09-26 10:51:59 +0200

    navbar default style corrected

--- components/navigation/links/SingleLinkItem.tsx
+++ components/navigation/links/SingleLinkItem.tsx
@@ -26,6 +26,7 @@ type Props = {
 const SingleLinkItem: React.FC<Props> = ({ item }) => {
   
   const router = useRouter();
+  console.log(item.current)
 
   const setNavCurrent = useStore(state => state.setNavCurrent)
   const scrollY = useStore(state => state.scrollY);
@@ -59,8 +60,8 @@ const SingleLinkItem: React.FC<Props> = ({ item }) => {
       >
       <motion.a
         className={classNames(
-          item.current ? 'after:scale-x-100' : 'after:hover:scale-x-100 after:hover:transition after:hover:ease-in after:hover:duration-300',
-          'px-3 py-2 font-large relative inline-block after:content-[""] after:absolute after:w-full after:h-1 after:bg-raisinBlack after:bottom-0 after:left-0 after:origin-top-left after:scale-x-0 after:scale-y-100'
+          item.current ? 'after:scale-x-100' : 'after:scale-x-0 after:hover:scale-x-100 after:hover:transition after:hover:ease-in after:hover:duration-300',
+          'px-3 py-2 font-large relative inline-block after:content-[""] after:absolute after:w-full after:h-1 after:bg-raisinBlack after:bottom-0 after:left-0 after:origin-top-left after:scale-y-100'
         )}
         aria-current={item.current ? 'page' : undefined}
       >
