9e663b11c7011d7bd62518aa48a32e0e0bcf190b
M-123-456 <mikigerlach2@gmail.com>
2022-09-27 14:05:47 +0200

    email style fixed

--- components/sections/contact/Email.tsx
+++ components/sections/contact/Email.tsx
@@ -34,25 +34,15 @@ const Email = () => {
         </p>
 
         <p
-            className="mt-4 mx-[8rem] text-lg text-center md:text-3xl relative after:content-[''] after:absolute after:w-full after:h-1 after:bg-raisinBlack after:bottom-0 after:left-0 after:origin-top-center after:scale-y-100 after:scale-x-0 after:hover:scale-x-100 after:transition after:ease-in after:duration-300"
-            onClick={() => copyToClipboard(authorData.email)}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
+            className="mt-4 text-lg flex justify-center md:text-3xl"
         >
-            <a href={`mailto:${authorData.email}`}>
+            <a 
+                className="text-center relative after:content-[''] after:absolute after:w-full after:h-1 after:bg-raisinBlack after:bottom-0 after:left-0 after:origin-top-center after:scale-y-100 after:scale-x-0 after:hover:scale-x-100 after:transition after:ease-in after:duration-300"
+                href={`mailto:${authorData.email}`}
+            >
                 {authorData.email}
             </a>
         </p>
-        {/* {
-            isHovered &&
-            <div 
-                className=" md:w-32 md:h-32 md:flex md:justify-center md:items-center md:rounded-full md:bg-raisinBlack md:opacity-70 md:text-white md:font-bold"
-            >
-                <div className="text-center">
-                    Click to Copy
-                </div>
-            </div>
-        } */}
     </div>
   )
 }
