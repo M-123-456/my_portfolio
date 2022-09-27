12cb029cd4a94ce72eae8e990ea1fc2cac3edb55
M-123-456 <mikigerlach2@gmail.com>
2022-09-27 14:10:06 +0200

    pageWrapper deleted

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ParallaxProvider } from 'react-scroll-parallax';
-import PageWrapper from '../components/wrapper/PageWrapper'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ParallaxProvider>
       <Component {...pageProps} />
-    </ParallaxProvider>
   )
 }
 
